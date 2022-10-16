6f09e5242e2ea8c9552f554c578cabcabd85b345
smartchaoqi <qcq11111111@163.com>
2022-10-16 20:40:24 +0800

    文章列表

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -0,0 +1,40 @@
+package com.heima.article.controller.v1;
+
+import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/api/v1/article")
+public class ArticleHomeController {
+
+    @Resource
+    private ApArticleService apArticleService;
+
+    /**
+     * 加载首页
+     * @param articleHomeDto
+     * @return
+     */
+    @PostMapping("/load")
+    public ResponseResult load(@RequestBody ArticleHomeDto articleHomeDto){
+        return apArticleService.load(articleHomeDto, ArticleConstants.LOADTYPE_LOAD_MORE);
+    }
+
+    @PostMapping("/loadmore")
+    public ResponseResult loadMore(@RequestBody ArticleHomeDto dto) {
+        return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_MORE);
+    }
+
+    @PostMapping("/loadnew")
+    public ResponseResult loadNew(@RequestBody ArticleHomeDto dto) {
+        return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_NEW);
+    }
+}

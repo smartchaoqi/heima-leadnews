50a0a82134b6a428dd8f4e5bae47b86e338784ac
smartchaoqi <qcq11111111@163.com>
2022-10-17 19:48:53 +0800

    集成miniostarter

--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -1,25 +1,48 @@
 package com.heima.minio.test;
 
+import com.heima.file.service.FileStorageService;
+import com.heima.minio.MinIoApplication;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+@SpringBootTest(classes = MinIoApplication.class)
+@RunWith(SpringRunner.class)
 public class MinIOTest {
-    public static void main(String[] args) {
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream("D:\\list.html");
-            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
-            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-                    .object("list.html")//文件名
-                    .contentType("text/html")//文件类型
-                    .bucket("leadnews")//桶名词  与minio创建的名词一致
-                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
-                    .build();
-            minioClient.putObject(putObjectArgs);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void test() throws IOException {
+        String path = fileStorageService.uploadHtmlFile("", "lista.html", Files.newInputStream(Paths.get("D:\\list.html")));
+        System.out.println(path);
     }
+
+
+//    public static void main(String[] args) {
+//        FileInputStream fileInputStream = null;
+//        try {
+//            fileInputStream = new FileInputStream("D:\\list.html");
+//            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
+//            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+//                    .object("list.html")//文件名
+//                    .contentType("text/html")//文件类型
+//                    .bucket("leadnews")//桶名词  与minio创建的名词一致
+//                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
+//                    .build();
+//            minioClient.putObject(putObjectArgs);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }

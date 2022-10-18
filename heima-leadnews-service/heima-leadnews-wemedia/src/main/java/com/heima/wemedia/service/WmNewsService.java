caed1f3db7f35cb65abca29d5e38663a0862a292
smartchaoqi <qcq11111111@163.com>
2022-10-18 21:43:19 +0800

    文章列表

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
@@ -0,0 +1,17 @@
+package com.heima.wemedia.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.model.wemedia.pojos.WmNews;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface WmNewsService extends IService<WmNews> {
+
+    /**
+     * 条件查询文章列表
+     * @param dto
+     * @return
+     */
+    public ResponseResult findAll(WmNewsPageReqDto dto);
+}
---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.heima.wemedia.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.wemedia.mapper.WmNewsMapper;
+import com.heima.wemedia.service.WmNewsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional
+public class WmNewsServiceImpl  extends ServiceImpl<WmNewsMapper, WmNews> implements WmNewsService {
+
+    @Override
+    public ResponseResult findAll(WmNewsPageReqDto dto) {
+        //检查参数
+        dto.checkParam();
+        //分页条件查询
+
+        //返回结果
+        return null;
+    }
+}

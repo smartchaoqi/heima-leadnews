587d238f964635fa16e82805024323bf80f652d9
smartchaoqi <qcq11111111@163.com>
2022-10-18 20:43:43 +0800

    分页查询素材

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
@@ -1,9 +1,13 @@
 package com.heima.wemedia.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.file.service.FileStorageService;
+import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
 import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.utils.thread.WmThreadLocalUtil;
 import com.heima.wemedia.mapper.WmMaterialMapper;
@@ -54,5 +58,24 @@ public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMater
         save(wmMaterial);
         //4.返回结果
         return ResponseResult.okResult(wmMaterial);
+    }
+
+    @Override
+    public ResponseResult findList(WmMaterialDto dto) {
+        //1.检查参数
+        dto.checkParam();
+        //2.分页查询
+        Page<WmMaterial> page = new Page<>(dto.getPage(),dto.getSize());
+        LambdaQueryWrapper<WmMaterial> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(WmMaterial::getUserId,WmThreadLocalUtil.getUser().getId());
+        if (dto.getIsCollection()!=null&&dto.getIsCollection()==1) {
+            wrapper.eq(WmMaterial::getIsCollection, dto.getIsCollection());
+        }
+        wrapper.orderByDesc(WmMaterial::getCreatedTime);
+        page(page,wrapper);
+        //3.结果返回
+        PageResponseResult pageResponseResult = new PageResponseResult(dto.getPage(),dto.getSize(),(int)page.getTotal());
+        pageResponseResult.setData(page.getRecords());
+        return pageResponseResult;
     }
 }

54284ef3bbc485ba915bfa3551dc3ca3f4150950
smartchaoqi <qcq11111111@163.com>
2022-10-18 22:17:39 +0800

    文章列表

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -1,13 +1,20 @@
 package com.heima.wemedia.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.utils.thread.WmThreadLocalUtil;
 import com.heima.wemedia.mapper.WmNewsMapper;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +25,41 @@ public class WmNewsServiceImpl  extends ServiceImpl<WmNewsMapper, WmNews> implem
 
     @Override
     public ResponseResult findAll(WmNewsPageReqDto dto) {
-        //检查参数
+        //1.检查参数
+        if(dto == null){
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //分页参数检查
         dto.checkParam();
+        //获取当前登录人的信息
+        WmUser user = WmThreadLocalUtil.getUser();
+        if(user == null){
+            return ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+        }
         //分页条件查询
-
+        Page<WmNews> page = new Page<>(dto.getPage(),dto.getSize());
+        LambdaQueryWrapper<WmNews> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(WmNews::getUserId,user.getId());
+        wrapper.orderByDesc(WmNews::getCreatedTime);
+        if (dto.getChannelId()!=null){
+            wrapper.eq(WmNews::getChannelId,dto.getChannelId());
+        }
+        if (dto.getStatus()!=null){
+            wrapper.eq(WmNews::getStatus,dto.getStatus());
+        }
+        //时间范围查询
+        if(dto.getBeginPubDate()!=null && dto.getEndPubDate()!=null){
+            wrapper.between(WmNews::getPublishTime,dto.getBeginPubDate(),dto.getEndPubDate());
+        }
+        if (StringUtils.isNotBlank(dto.getKeyword())){
+            wrapper.like(WmNews::getTitle,dto.getKeyword());
+        }
+        page(page,wrapper);
         //返回结果
-        return null;
+
+        ResponseResult responseResult = new PageResponseResult(dto.getPage(),dto.getSize(),(int)page.getTotal());
+        responseResult.setData(page.getRecords());
+
+        return responseResult;
     }
 }

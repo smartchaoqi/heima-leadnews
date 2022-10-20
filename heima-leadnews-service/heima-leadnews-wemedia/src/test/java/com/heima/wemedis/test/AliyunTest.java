cee19ec5ccc94dbc45ccc2bf8419e67af63cda93
smartchaoqi <qcq11111111@163.com>
2022-10-20 14:58:37 +0800

    集成阿里云图片审核和内容审核

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedis/test/AliyunTest.java
@@ -0,0 +1,41 @@
+package com.heima.wemedis.test;
+
+import com.heima.common.aliyun.GreenImageScan;
+import com.heima.common.aliyun.GreenTextScan;
+import com.heima.file.service.FileStorageService;
+import com.heima.wemedia.WemediaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootTest(classes = WemediaApplication.class)
+@RunWith(SpringRunner.class)
+public class AliyunTest {
+
+    @Autowired
+    private GreenTextScan greenTextScan;
+
+    @Autowired
+    private GreenImageScan greenImageScan;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void testScanTest() throws Exception {
+        Map map = greenTextScan.greeTextScan("我是一个好人,冰毒");
+        System.out.println(map);
+    }
+
+    @Test
+    public void testScanImage() throws Exception {
+        byte[] bytes = fileStorageService.downLoadFile("http://192.168.200.130:9000/leadnews/2021/04/26/a73f5b60c0d84c32bfe175055aaaac40.jpg");
+        greenImageScan.imageScan(Collections.singletonList(bytes));
+    }
+}
